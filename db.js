a0cdb4e1f2870d473107284ef1c88b143d20e327
chauhanmanesh <chauhanmanesh19@gmail.com>
2026-04-09 17:22:20 +0530

    fix db connection

--- db.js
+++ db.js
@@ -1,15 +1,17 @@
 require('dotenv').config();
 const mysql = require('mysql2');
+require('dotenv').config();
+
 
 const pool = mysql.createPool({
-  host: 'localhost',
-  user:'root',
-  password: process.env.db_password,
-  database:'School_Management'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
 });
 
 const promisePool = pool.promise();
-
 
 promisePool.getConnection()
   .then(connection => {
