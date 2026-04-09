e94a34ff700b57dc0a5060a6d295830fc97e0eb2
Chauhan-Vishakhaa <chauhanvishakha11@gmail.com>
2026-04-09 22:30:27 +0530

    Update server.js

--- server.js
+++ server.js
@@ -62,6 +62,7 @@ app.get('/listSchools',validateListSchools, async (req, res) => {
 
 
 
-app.listen(PORT, ()=> {
-    console.log("Server is Running")
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running on port ${PORT}`);
 });
